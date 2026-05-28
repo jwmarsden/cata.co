f6c26aff68d0f3ad12f7866a81c3f0d8dc4365da
J.W.Marsden <j.w.marsden@gmail.com>
2026-05-28 19:56:34 +0930

    bigger files

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,8 @@
-import { index_posts } from '$lib/server/posts/post_index';
+import { sequence } from '@sveltejs/kit/hooks';
 import { handle as authHandle } from './auth';
 import { redirect } from '@sveltejs/kit';
 import type { Handle, ServerInit } from '@sveltejs/kit';
-import { sequence } from '@sveltejs/kit/hooks';
+import { index_posts } from '$lib/server/posts/post_index';
 
 export const init: ServerInit = async () => {
 	index_posts().then(() => {
@@ -10,25 +10,18 @@ export const init: ServerInit = async () => {
 	}).catch(err => {
 		console.error('Error indexing posts:', err);
 	});
-
 };
 
 const adminGuard: Handle = async ({ event, resolve }) => {
-	//console.log('PATH:', event.url.pathname);
-
 	if (event.url.pathname.startsWith('/admin')) {
 		if (event.url.pathname.startsWith('/admin/login')) {
-			//console.log('ALLOWING login page through');
 			return resolve(event);
 		}
 		const session = await event.locals.auth();
-		//console.log('SESSION:', session);
 		if (!session?.user) {
-			//console.log('NO SESSION, redirecting to login');
 			redirect(303, '/admin/login');
 		}
 	}
-
 	return resolve(event);
 };
 
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,10 @@
 import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
+  preprocess: vitePreprocess({
+			bodySize: 50 * 1024 * 1024  // 50MB
+		}),
   kit: {
     adapter: adapter(),
     typescript: {
