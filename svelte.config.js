42498c092b94d6fe5a61df513907e2fa86334494
J.W.Marsden <j.w.marsden@gmail.com>
2026-05-28 19:59:10 +0930

    bigger files

--- svelte.config.js
+++ svelte.config.js
@@ -2,11 +2,11 @@ import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
-  preprocess: vitePreprocess({
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
 			bodySize: 50 * 1024 * 1024  // 50MB
 		}),
-  kit: {
-    adapter: adapter(),
     typescript: {
       config: (config) => ({
         ...config,
